91f7a46d1b9299e76125534f9d284a2961917f57
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2025-01-12 15:58:37 +0000

    add account types

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_12_141030) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_12_154347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "account_types", force: :cascade do |t|
+    t.string "name"
+    t.string "alias"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clients", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "first_name"
