ba47708af58d9caa5ce5bec3c28b5ca101aa6034
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2025-01-12 13:49:52 +0000

    - add acitve_model_Serializers - configure jwt - Add user model - add login endpoint

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,13 @@
+class AuthController < ApplicationController
+  # POST request 'auth/login'
+  def login
+    @user = User.find_by(email: params[:email])
+
+    if @user&.authenticate(params[:password])
+      token =  jwt_encode(user_id: @user.id)
+      render json: {user: UserSerializer.new(@user), token: token}, status: :ok
+    else
+      render json: {error: 'Invalid email or password'}, status: :unauthorized
+    end
+  end
+end
